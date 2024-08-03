6b4f67f19b5c3ba99f8058e958cfaeebb420a88c
MaorShmueli55 <maorshmueli5@gmail.com>
2024-08-03 23:30:05 +0300

    11

--- 31.07.2024/main.js
+++ 31.07.2024/main.js
@@ -297,3 +297,303 @@ pet.getStatus();
 
 
 
+
+
+
+
+
+///////////////////////////////////////////
+//30 Basic Nested Loop Exercises in JavaScript
+//1
+function printStr(str , num){
+    let row = ""
+    for(let i = 0; i < num; i++){
+        for(let j = 0; j < num; j++){
+            row += str + " ";
+        }
+        row += `\n`
+    }
+    console.log(row);
+}
+
+
+printStr("*" , 3);
+
+//2
+function sum(num){
+    let sum1;
+    let row = "";
+    for(let i = 0; i < num; i++){
+        for(let j = 0; j < num; j++){
+            sum1 = i + j
+            row += sum1.toString() + " ";
+        }
+        row += `\n`
+    }
+    console.log(row);
+}
+
+sum(5);
+
+//3
+function printStr1(str , num){
+    let row = ""
+    for(let i = 0; i < num; i++){
+        for(let j = 0; j < i; j++){
+            row += str + " ";
+        }
+        row += `\n`
+    }
+    console.log(row);
+}
+
+
+printStr1("*" , 5);
+
+//4
+function mult(num){
+    let sum1;
+    let row = "";
+    for(let i = 1; i <= num; i++){
+        for(let j = 1; j <= num; j++){
+            sum1 = i * j
+            row += sum1.toString() + " ";
+        }
+        row += `\n`
+    }
+    console.log(row);
+}
+
+mult(10);
+
+//5
+function squareBorder(str , num){
+    let row = "";
+    for(let i = 0 ; i < num; i ++){
+        for(let j = 0; j < num; j++){
+            if(i === 0 || i === (num-1) || (j=== 0 || j === (num - 1))){
+                row += str + " ";
+            }
+            else{
+                row += "  ";
+            }
+        }
+        row += `\n`
+    }
+    console.log(row);
+}
+
+squareBorder("$" , 9);
+
+//6
+function numFromTo(){
+    let row = "";
+    let sum = 0
+    for(let i = 1; i < 6; i++){
+        for(let j  = 0; j < 5; j++){
+            if(i <= 2){
+            sum += 1
+            row += `${sum.toString()}  `
+            }
+            else{
+            sum += 1
+            row += `${sum.toString()} `
+            }
+        }
+        row += `\n`;
+    }
+    console.log(row);
+}
+
+numFromTo();
+
+// 7
+console.log("\nExercise 7:");
+for (let i = 1; i <= 5; i++) {
+    let row = ' '.repeat(5 - i);
+    for (let j = 1; j <= i; j++) {
+        row += i + ' ';
+    }
+    console.log(row);
+}
+
+// 8
+console.log("\nExercise 8:");
+for (let i = 0; i < 5; i++) {
+    let row = '';
+    for (let j = 0; j < 5; j++) {
+        row += ((i + j) % 2) + ' ';
+    }
+    console.log(row);
+}
+
+// 9
+console.log("\nExercise 9:");
+let pascal = [[1]];
+for (let i = 1; i < 5; i++) {
+    let row = [1];
+    for (let j = 1; j < i; j++) {
+        row.push(pascal[i-1][j-1] + pascal[i-1][j]);
+    }
+    row.push(1);
+    pascal.push(row);
+}
+pascal.forEach((row, i) => {
+    console.log(' '.repeat(4-i) + row.join(' '));
+});
+
+// 10
+console.log("\nExercise 10:");
+for (let i = 0; i < 5; i++) {
+    let row = '';
+    for (let j = 0; j < 5; j++) {
+        if (i === 0 || i === 4 || j === 0 || j === 4) {
+            row += '* ';
+        } else {
+            row += '  ';
+        }
+    }
+    console.log(row);
+}
+
+// 11
+console.log("\nExercise 11:");
+count = 1;
+for (let i = 1; i <= 5; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+        row += count + ' ';
+        count++;
+    }
+    console.log(row);
+}
+
+// 12
+console.log("\nExercise 12:");
+for (let i = 1; i <= 4; i++) {
+    let row = '';
+    for (let j = 1; j <= 4; j++) {
+        row += i.toString().repeat(j) + ' ';
+    }
+    console.log(row);
+}
+
+//13
+console.log("\nExercise 13:");
+for (let i = 5; i >= 1; i--) {
+    console.log('* '.repeat(i));
+}
+
+// 14
+console.log("\nExercise 14:");
+for (let i = 0; i < 5; i++) {
+    let row = '';
+    for (let j = 0; j < 5; j++) {
+        row += Math.max(i, j) + ' ';
+    }
+    console.log(row);
+}
+
+// 15
+console.log("\nExercise 15:");
+for (let i = 1; i <= 5; i += 2) {
+    console.log(' '.repeat((5-i)/2) + '*'.repeat(i));
+}
+for (let i = 3; i >= 1; i -= 2) {
+    console.log(' '.repeat((5-i)/2) + '*'.repeat(i));
+}
+
+// 16
+console.log("\nExercise 16:");
+for (let i = 0; i < 6; i++) {
+    let row = '';
+    for (let j = 0; j < 6; j++) {
+        row += (i * j) + ' ';
+    }
+    console.log(row);
+}
+
+//17
+console.log("\nExercise 17:");
+let char = 65;
+for (let i = 1; i <= 5; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+        row += String.fromCharCode(char) + ' ';
+        char++;
+    }
+    console.log(row);
+}
+
+//18
+console.log("\nExercise 18:");
+let spiral = Array(5).fill().map(() => Array(5).fill(0));
+let num = 1, top = 0, bottom = 4, left = 0, right = 4;
+
+while (num <= 25) {
+    for (let i = left; i <= right; i++) spiral[top][i] = num++;
+    top++;
+    for (let i = top; i <= bottom; i++) spiral[i][right] = num++;
+    right--;
+    for (let i = right; i >= left; i--) spiral[bottom][i] = num++;
+    bottom--;
+    for (let i = bottom; i >= top; i--) spiral[i][left] = num++;
+    left++;
+}
+
+spiral.forEach(row => console.log(row.map(n => n.toString().padStart(2, ' ')).join(' ')));
+
+//19
+console.log("\nExercise 19:");
+for (let i = 1; i <= 5; i++) {
+    let row = ' '.repeat(5-i);
+    for (let j = 1; j <= 2*i-1; j++) {
+        if (j === 1 || j === 2*i-1 || i === 5) row += '*';
+        else row += ' ';
+    }
+    console.log(row);
+}
+
+//20
+console.log("\nExercise 20:");
+function sumTo(n) {
+    return (n * (n + 1)) / 2;
+}
+for (let i = 0; i < 4; i++) {
+    let row = '';
+    for (let j = 0; j < 4; j++) {
+        row += sumTo(i * j) + ' ';
+    }
+    console.log(row);
+}
+
+//21
+console.log("\nExercise 21:");
+for (let i = 5; i >= 1; i -= 2) {
+    console.log(' '.repeat((5-i)/2) + '*'.repeat(i));
+}
+for (let i = 3; i <= 5; i += 2) {
+    console.log(' '.repeat((5-i)/2) + '*'.repeat(i));
+}
+
+//22
+console.log("\nExercise 22:");
+for (let i = 0; i < 5; i++) {
+    let row = '';
+    for (let j = 0; j < 5; j++) {
+        row += Math.abs(i - j) + ' ';
+    }
+    console.log(row);
+}
+
+//23
+console.log("\nExercise 23:");
+count = 2;
+for (let i = 1; i <= 5; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+        row += count + ' ';
+        count += 2;
+    }
+    console.log(row);
+}
