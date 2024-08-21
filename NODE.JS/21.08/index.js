bc56c146553966c4c592e9332841a90cc811d485
MaorShmueli55 <maorshmueli5@gmail.com>
2024-08-21 13:53:06 +0300

    1

--- NODE.JS/21.08/index.js
+++ NODE.JS/21.08/index.js
@@ -1,1 +1,50 @@
 console.log("baba!");
+const express = require("express")
+const app = express()
+const contact = {email : "maor@1234", phone: "050000000"}
+const product = [
+    {id: "1",
+     name: "iphone",
+     price: 4500   
+    },
+    {
+        id: "2",
+        name: "galaxy",
+        price: 4000
+    },
+    {
+        id:"3",
+        name: "nokiya",
+        price : 300
+    }
+];
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my basic Express server!")
+});
+
+app.get("/about", (req, res) => {
+    res.json("This server was created by Maor")
+});
+
+app.get("/contact", (req, res) => {
+    res.json(contact)
+});
+
+
+app.get("/api/product", (req, res) => {
+    res.send(product)
+});
+
+app.get("/api/product/:productID", (req, res) => {
+    const productId = req.params.productID;
+    for(let i = 0; i < product.length; i++){
+        if(product[i].id === productId)  res.json(product[i])
+            else res.send ("product not found")
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+  });
