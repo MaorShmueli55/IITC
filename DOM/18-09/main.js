47209bc14c2878c280992cc974e0392513d51693
MaorShmueli55 <maorshmueli5@gmail.com>
2024-09-22 09:06:12 +0300

    99

--- DOM/18-09/main.js
+++ DOM/18-09/main.js
@@ -6,13 +6,16 @@ let gToDoList = [
     }
 ];
 
-
 const elForm = document.querySelector("form");
 elForm.addEventListener("submit" , function (ev) {
     ev.preventDefault();
     const taskValue = elForm.querySelector("#task").value;
-
+    if(taskValue){
     createTask(taskValue);
+    }
+    else{
+        alert("Task can't be empty")
+    }
 })
 
 function makeId() {
@@ -25,7 +28,18 @@ function makeId() {
     return id;
   }
 
+//   function renderToDoList(){
+//   gToDoList.push(newTask);
+//   const elToDoLIst = document.getElementById("toDoList");
+//   const elTask = document.createElement("li");
+//   elTask.setAttribute("id" , "el" + task.id);
 
+//   elTask.innerHTML = `
+//   <div>${task.task}</div>
+//   <button onclick="deleteTask('${task.id}')">Delete</button>
+//   `;
+//   elToDoLIst.appendChild(elTask);
+//   }
 
   function renderToDoList(){
     const elToDoLIst = document.getElementById("toDoList");
@@ -37,7 +51,7 @@ function makeId() {
 
         elTask.innerHTML = `
         <div>${task.task}</div>
-        <button onclick="deleteTask('${task.id}')">Delete</button>
+        <button onclick="deleteTask('${task.id}')">&#128465</button>
         `;
         elToDoLIst.appendChild(elTask);
     }
@@ -68,10 +82,34 @@ function createTask(task){
         isDone: false
     };
 
+    elForm.querySelector("#task").value = "";
+
     gToDoList.push(newTask);
     const elToDoLIst = document.getElementById("toDoList");
     elToDoLIst.innerHTML = "";
     renderToDoList();
 }
 
-renderToDoList();
+
+
+
+const elToDoLIst = document.getElementById("toDoList")
+elToDoLIst.addEventListener("click", function (ev) {
+    // ev.target.isDone = true;
+    selectItem(ev.target);
+});
+
+
+function selectItem(currentItem) {   
+    if(!currentItem.isDone){
+            currentItem.classList.add("selected");
+            currentItem.isDone = true;
+    }
+    else{
+        currentItem.classList.remove("selected")
+        currentItem.isDone = false;
+    }
+        }
+
+    
+    renderToDoList();
