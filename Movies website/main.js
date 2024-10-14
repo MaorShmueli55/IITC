a52567d0b8ace62fd593c7cd949007040783b852
MaorShmueli55 <maorshmueli5@gmail.com>
2024-10-14 20:22:26 +0300

    done

---
+++ Movies website/main.js
@@ -0,0 +1,37 @@
+import {API_KEY} from "./secret.js";
+const containerEl = document.querySelector(".container");
+
+const saveTheData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+      );
+      renderMovie(response.data.results);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  
+  saveTheData();
+
+  function renderMovie(data) {
+    data.forEach(movie => {
+        containerEl.innerHTML += `
+<div class="movieCard">
+    <div class="movieInner">
+        <div class="movieFront">
+            <img class = "imgCard" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="">
+            <p>${movie.title}</p>
+            <div class="rate"> <span>Rating:</span> ${movie.vote_average}</div>
+            <button class="fav">Favorites</button>
+        </div>
+        <div class="movieBack">
+            <div class="movieAbout">${movie.overview}</div>
+        </div>
+    </div>
+</div>
+        `
+    });
+    console.log(data);
+  }
+
